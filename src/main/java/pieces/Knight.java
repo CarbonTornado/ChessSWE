06ee6472297cdf93a1d893646c6393c4ea2b2c44
D073415 <t.kalkhof@sap.com>
2021-05-25 15:51:41 +0200

    Added Legal Moves

--- src/main/java/pieces/Knight.java
+++ src/main/java/pieces/Knight.java
@@ -4,6 +4,7 @@ import board.Board;
 import board.Square;
 
 import java.awt.image.BufferedImage;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Knight extends Piece{
@@ -14,12 +15,27 @@ public class Knight extends Piece{
      * @param image      the image
      * @param initSquare the init square
      */
-    protected Knight(PieceColor color, BufferedImage image, Square initSquare) {
+    public Knight(PieceColor color, BufferedImage image, Square initSquare) {
         super(color, image, initSquare);
     }
 
     @Override
     public List<Square> getLegalMoves(Board b) {
-        return null;
+        LinkedList<Square> legalMoves = new LinkedList<>();
+
+        Square[][] squares = b.getSquares();
+
+        int x = this.getPosition().getXPos();
+        int y = this.getPosition().getYPos();
+
+        for (int xOffset = 2; xOffset >= -2; xOffset--) {
+            for (int yOffset = 3; yOffset >= -3; yOffset--) {
+                if ((x + xOffset >= 0 && y + yOffset >= 0) && (x + xOffset < squares.length && y + yOffset < squares.length) && Math.abs(xOffset) == 2 ^ Math.abs(yOffset) == 2) {
+                        legalMoves.add(squares[x + xOffset][y + yOffset]);
+                }
+            }
+        }
+
+        return legalMoves;
     }
 }
--- src/main/java/pieces/Pawn.java
+++ src/main/java/pieces/Pawn.java
@@ -4,9 +4,12 @@ import board.Board;
 import board.Square;
 
 import java.awt.image.BufferedImage;
+import java.util.LinkedList;
 import java.util.List;
 
-public class Pawn extends Piece{
+public class Pawn extends Piece {
+    public boolean positionIsInitial = true;
+
     /**
      * Instantiates a new Piece.
      *
@@ -14,12 +17,42 @@ public class Pawn extends Piece{
      * @param image      the image
      * @param initSquare the init square
      */
-    protected Pawn(PieceColor color, BufferedImage image, Square initSquare) {
+    public Pawn(PieceColor color, BufferedImage image, Square initSquare) {
         super(color, image, initSquare);
     }
 
     @Override
+    public boolean move(Square arrivalSquare) {
+        positionIsInitial = false;
+        return super.move(arrivalSquare);
+    }
+
+    @Override
     public List<Square> getLegalMoves(Board b) {
-        return null;
+        //TODO: Add Test for bug for not having legal move
+        LinkedList<Square> legalMoves = new LinkedList<>();
+
+        Square[][] squares = b.getSquares();
+
+        int x = this.getPosition().getXPos();
+        int y = this.getPosition().getYPos();
+        //Code Smell Duplicate code
+        if (this.getColor() == PieceColor.WHITE){
+            if (positionIsInitial) {
+                legalMoves.add(squares[x][y-2]);
+            }
+            if (!squares[x][y-1].isOccupied()){
+                legalMoves.add(squares[x][y-1]);
+            }
+        }
+        else{
+            if (positionIsInitial) {
+                legalMoves.add(squares[x][y+2]);
+            }
+            if (!squares[x][y+1].isOccupied()) {
+                legalMoves.add(squares[x][y+1]);
+            }
+        }
+        return legalMoves;
     }
 }
