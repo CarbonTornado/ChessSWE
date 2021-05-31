00b0b4279da01c81a256cb2ccbb0bcc7b6f6c1d3
D073415 <t.kalkhof@sap.com>
2021-05-31 19:21:57 +0200

    Added Mock for ImageLoader Test

--- src/test/java/utils/ImageLoaderTest.java
+++ src/test/java/utils/ImageLoaderTest.java
@@ -1,5 +1,6 @@
 package utils;
 import org.junit.Test;
+import org.powermock.api.mockito.PowerMockito;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -7,17 +8,24 @@ import java.io.File;
 import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.powermock.api.mockito.PowerMockito.when;
 
-//Integration Test
 public class ImageLoaderTest {
 
-
-    //TODO: Test weird edge case where a random image is loaded is resource does not exist
-    //TODO: Fix and rework
-    /// @Test
+    @Test
     public void testLoadImage() throws IOException {
-        BufferedImage bufferedImage = ImageIO.read(new File(System.getProperty("user.dir") + "/src/main/resources/Chess_kdt60.png"));
+        PowerMockito.mockStatic(ImageIO.class);
 
-        assertThat(ImageLoader.loadImage(System.getProperty("user.dir") + "/src/main/resources/Chess_kdt60.png")).isEqualTo(bufferedImage);
+        when(ImageIO.read((File) any())).thenAnswer(invocation -> {
+            Object argument = invocation.getArguments()[0];
+            // here you can check what arguments you were passed
+
+            // here you can fill in some data so the image isn't blank
+
+            return new BufferedImage(600, 400, BufferedImage.TYPE_INT_RGB);
+        });
+
+        assertThat(ImageLoader.loadImage(null)).isEqualTo(new BufferedImage(600, 400, BufferedImage.TYPE_INT_RGB));
     }
 }
